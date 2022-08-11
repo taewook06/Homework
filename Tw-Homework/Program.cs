b6c0d6b3bb1cf00cfdb8dc8fed16c560aff341c4
taewook06 <109446725+taewook06@users.noreply.github.com>
2022-08-11 14:24:53 +0900

    1

--- Tw-Homework/Program.cs
+++ Tw-Homework/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-
-namespace Tw_Homework
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-
-        }
-    }
-}
-
-
-
+﻿
 
